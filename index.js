83363bdff15b3040677d39715b79a81dec47aca6
krish-vijayan <krishvijayan55@gmail.com>
2022-06-22 17:01:00 -0400

    Changed PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const PORT = 8080;
+const { PORT = 8080 } = process.env;
 const data = require('./group-data');
 
 app.use(bodyParser.json()).use(cors());
