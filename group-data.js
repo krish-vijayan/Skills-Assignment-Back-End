0c7119085895dd7eab841443e89d106b3ad7486c
krish-vijayan <krishvijayan55@gmail.com>
2022-06-23 10:49:39 -0400

    Added Brandon, Serena, Taeyun

--- group-data.js
+++ group-data.js
@@ -1,6 +1,8 @@
+ 
  module.exports = {
     AllMembers: [
         {
+            id:0, //this id is used for api call (should be equal to index number)
             name: "Krish Vijayan",
             pfp: "../images/pfp.JPG",
             uwLogo: "../images/uw.png",
@@ -11,7 +13,41 @@
             interests: ["->Programming", "->NBA", "->Computers","->Peripherals","->Technology"],
             qr: "../images/qr.jpg",
             link: "https://www.linkedin.com/in/krish-vijayan-617263219",
-        }
+        },
+        {
+            id:1,
+            name: "Brandon Wang",
+            photo: "../images/pfp.JPG",
+            school: "../images/school.JPG",
+            program: "Honours Mathematics",
+            interests: "Anime, Games, Jpop",
+            linkedin: "https://www.linkedin.com/in/bw856/",
+            github: "https://github.com/bw856",
+        },
+        {
+            id:2,
+            name: "Serena Wittenberg",
+            pfp: "../images/pfp.png",
+            uwLogo: "../images/uw.png",
+            school: "University of Waterloo",
+            program: "Biomedical Engineering",
+            hobbies: ["Chess","Swimming", "Programming", "Design"],
+            interests: ["Video Editing", "IOS Development", "Piano"],
+            qr: "../images/qr.png",
+            link: "https://www.linkedin.com/in/serena-wittenberg",
+        },
+        {
+            id:3,
+            name: "Taeyun Kim",
+            pfp: "../images/pfp.JPG",
+            uwLogo: "../images/uw.png",
+            school: "University of Waterloo",
+            program: "Honours Mathematics",
+            hobbies: ["-> Gaming","-> Cooking", "-> Swimming","-> Travelling"],
+            interests: ["-> Car", "-> Music", "-> E-Sports","-> Stock","-> Technology"],
+            qr: "../images/qr.JPG",
+            link: "https://www.linkedin.com/in/taeyun-kim-ba18291b7",
+        },
     ]
  };
  
